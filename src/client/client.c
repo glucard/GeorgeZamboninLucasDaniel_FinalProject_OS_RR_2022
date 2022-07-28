630026c21c5af6a91a48e2acff45519c3d2a6fb1
George <georgezambonin@gmail.com>
2022-07-27 21:29:24 -0400

    --log filename added.

--- src/client/client.c
+++ src/client/client.c
@@ -9,17 +9,35 @@
 
 #define MAX 80
 #define SA struct sockaddr
+
+int is_log = 0;
+char log_filename[MAX];
+#include <time.h>
+
+void logger(char* s){
+    if (is_log) {
+        FILE* log_file = fopen(log_filename, "a+");
+        time_t t = time(NULL);
+        struct tm tm = *localtime(&t);
+        fprintf(log_file, "%d-%02d-%02d %02d:%02d:%02d ", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+        fprintf(log_file, "%s", s);
+        fclose(log_file);
+    }
+}
+
 void func(int sockfd)
 {
     char buff[MAX];
     int n;
     for (;;) {
         bzero(buff, sizeof(buff));
-        printf("Enter the string : ");
+        printf("Enter the string: ");
         n = 0;
         bzero(buff, sizeof(buff));
-        while ((buff[n++] = getchar()) != '\n')
-            ;
+
+        while ((buff[n++] = getchar()) != '\n');
+        
+        logger(buff);
         write(sockfd, buff, sizeof(buff));
         // read(sockfd, buff, sizeof(buff));
         // printf("From Server : %s", buff);
@@ -37,12 +55,23 @@ int main(int argsc, char **argsv)
     struct sockaddr_in servaddr, cli;
 
     int argsv_i = 1; // ignore ./filename
-
-    while(argsv_i != argsc){
-        if (strcmp(argsv[argsv_i++], "--port") == 0) {
-            port = atoi(argsv[argsv_i++]);
+    if(argsc > 2){
+        while(argsv_i < argsc){
+            if (strcmp(argsv[argsv_i], "--port") == 0) {
+                port = atoi(argsv[++argsv_i]);
+                continue;
+            }
+            if (strcmp(argsv[argsv_i], "--log") == 0) {
+                bzero(log_filename, MAX);
+                strcpy(log_filename, argsv[++argsv_i]);
+                is_log = 1;
+                continue;
+            }
+            if (strcmp(argsv[argsv_i], "--compress") == 0) {
+                continue;   
+            }
+            argsv_i++;
         }
-
     }
     // socket create and verification
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -74,4 +103,8 @@ int main(int argsc, char **argsv)
    
     // close the socket
     close(sockfd);
+    
+    // close log_file, if open.
+
+    return 0;
 }
